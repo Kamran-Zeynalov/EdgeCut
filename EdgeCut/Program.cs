e55c5823b2aa33b4c6301f0ba436f1053c3c3ad1
Kamran-Zeynalov <kamraneze@code.edu.az>
2024-07-16 11:48:22 +0400

    AdminPanel Done :knuddels_party:

--- EdgeCut/Program.cs
+++ EdgeCut/Program.cs
@@ -23,10 +23,18 @@ namespace EdgeCut
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+           
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+               name: "default",
+               pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
             app.Run();
         }
     }
